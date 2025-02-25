fab7d3a896ac074d007867ba7094dc121ccf416f
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-02-26 01:10:26 +0300

    fix: add the commands and variables.

--- lab3/src/task16/main.sh
+++ lab3/src/task16/main.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 declare USER=student
+declare FOLDER_NAME=/tmp
+declare FIRST_FILE_NAME=PAPKA1
+declare SECOND_FILE_NAME=PAPKA2
+declare TARGET_FIRST_FILE=$FOLDER_NAME/$FIRST_FILE_NAME
+declare TARGET_SECOND_FILE=$FOLDER_NAME/$FIRST_SECOND_NAME
 
 echo "sudo --user $USER" 
 sudo --user $USER bash << EOF
-	
+	echo "find 2>/dev/null $FOLDER_NAME -name $FIRST_FILE_NAME"
+	find 2>/dev/null $FOLDER_NAME -name $FIRST_FILE_NAME
+
+	echo "find 2>/dev/null $FOLDER_NAME -name $SECOND_FILE_NAME"
+	find 2>/dev/null /tmp -name $SECOND_FILE_NAME
+
+	echo "rm $TARGET_FIRST_FILE $TARGET_SECOND_FILE"
+	rm $TARGET_FIRST_FILE $TARGET_SECOND_FILE
+
+	echo "rm $TARGET_FIRST_FILE"
+	rm $TARGET_FIRST_FILE
+
+	echo "rm $TARGET_SECOND_FILE"
+	rm $TARGET_SECOND_FILE
 EOF

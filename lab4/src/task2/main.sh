646e464e562c7e6fdfc7bb7cb2d973844c775e9d
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-12 01:07:09 +0300

    fix: add the output only to file.

--- lab4/src/task2/main.sh
+++ lab4/src/task2/main.sh
@@ -3,6 +3,5 @@
 declare OUTPUT_FILE=task2.csv
 
 set -v
-find $HOME -type f -mmin -60 -ls | awk '{printf "%s,%s,%s,%s,\n", $8, $9, $10, $11}' | tee $OUTPUT_FILE
-
+find $HOME -type f -mmin -60 -ls | awk '{printf "%s,%s,%s,%s\n", $8, $9, $10, $11}' > $OUTPUT_FILE
 scrot -c -d 9 'task2.png' -e 'mv $f ./assets/';

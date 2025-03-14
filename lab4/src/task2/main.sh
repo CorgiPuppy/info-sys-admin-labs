c2524e2fae210ea0eb150c3baa84b416fa9a684d
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-14 18:06:44 +0300

    fix: update.

--- lab4/src/task2/main.sh
+++ lab4/src/task2/main.sh
@@ -5,6 +5,6 @@ declare OUTPUT_FILE_NAME=output.csv
 declare OUTPUT_FILE=$OUTPUT_DIR_NAME/$OUTPUT_FILE_NAME
 
 set -v
-$OUTPUT_FILE | find $HOME -type f -mmin -60 -ls | awk '{printf "%s,%s,%s,%s\n", $8, $9, $10, $11}' > $OUTPUT_FILE
+find $HOME -type f -mmin -60 -ls | awk '{printf "%s,%s,%s,%s\n", $8, $9, $10, $11}' > $OUTPUT_FILE
 
 scrot -c -d 2 'task2.png' -e 'mv $f ./assets/';

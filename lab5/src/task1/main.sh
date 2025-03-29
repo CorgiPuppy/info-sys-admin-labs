8ab095fc71f034d8d5ea89877dc67c4025d1a306
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-30 02:09:59 +0300

    fix: update.

--- lab5/src/task1/main.sh
+++ lab5/src/task1/main.sh
@@ -4,4 +4,7 @@ declare TARGET_DIR=/etc
 declare USER=root
 
 set -v
-find $TARGET_DIR -type f -user $USER 2> >(grep -v 'Permission denied$' 1>&2) -exec ls -l {} + | sort -nr -k5 | head -n 1 | awk '{print $9, $5}'
+find $TARGET_DIR -type f -user $USER 2> >(grep -v 'Permission denied$' 1>&2) -exec ls -l {} + | head -n 1 <(sort -nr -k5) | awk '{print $9, $5}'
+
+declare longest_file=$(find $TARGET_DIR -type f -user $USER 2> >(grep -v 'Permission denied$' 1>&2) -exec bash -c 'echo "$(basename "{}" | wc -c) {}"' \; | sort -nr | head -n 1 | cut -d' ' -f2-)
+stat -c "%s %n" "$longest_file"

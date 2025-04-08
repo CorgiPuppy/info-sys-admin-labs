3a970a3fb94036229940adb0a20a13f533e7acd4
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-09 00:40:49 +0300

    feat: add the execution of the task.

---
+++ lab6/src/task10/main.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function listFile {
+	echo "ls -l $1"
+	ls -l $1
+}
+
+declare -f listDir
+listDir() {
+	echo "ls -ld $1"
+	ls -ld $1
+}
+
+declare FOLDER_NAME=$HOME
+declare TARGET_DIR_NAME=test_dir
+declare TARGET_DIR=$FOLDER_NAME/$TARGET_DIR_NAME
+declare TARGET_FILE_NAME=file1.txt
+declare TARGET_FILE=$TARGET_DIR/$TARGET_FILE_NAME
+declare USER=u
+declare GROUP_OTHER=go
+declare -i RIGHTS_DIR=700
+declare RIGHTS_FILE_USER=rw
+declare RIGHTS_FILE_GROUP_OTHER=r
+
+echo "mkdir $TARGET_DIR"
+mkdir $TARGET_DIR
+
+listDir $TARGET_DIR
+
+echo "chmod $RIGHTS_DIR $TARGET_DIR"
+chmod $RIGHTS_DIR $TARGET_DIR
+
+listDir $TARGET_DIR
+
+echo "touch $TARGET_FILE"
+touch $TARGET_FILE
+
+listFile $TARGET_FILE
+
+echo "chmod $USER+$RIGHTS_FILE_USER,$GROUP_OTHER=$RIGHTS_FILE_GROUP_OTHER $TARGET_FILE"
+chmod $USER+$RIGHTS_FILE_USER,$GROUP_OTHER=$RIGHTS_FILE_GROUP_OTHER $TARGET_FILE
+
+listFile $TARGET_FILE
+
+rm $TARGET_FILE
+rmdir $TARGET_DIR

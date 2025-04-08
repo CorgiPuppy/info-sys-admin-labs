00ec0859e3edefde4a402634504852e759caba91
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-09 01:45:40 +0300

    feat: add the execution of the task.

---
+++ lab6/src/task13/main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function listFile {
+	echo "ls -l $1"
+	ls -l $1
+}
+
+declare -f getfaclFile 
+getfaclFile() {
+	echo "getfacl 2>/dev/null $1"
+	getfacl 2>/dev/null $1
+}
+
+declare FOLDER_NAME=$HOME
+declare TARGET_FILE_NAME=file1.txt
+declare TARGET_FILE=$FOLDER_NAME/$TARGET_FILE_NAME
+declare USER2=user2
+declare SHELL_USER2=/bin/bash
+declare CATALOG_USER2=/home/user2
+declare FOLDER_USERS=/etc/passwd
+declare USER=u
+
+sudo useradd -s $SHELL_USER2 -m -d $CATALOG_USER2 $USER2
+
+touch $TARGET_FILE
+
+setfacl -m u:$USER2:w $TARGET_FILE
+
+listFile $TARGET_FILE
+getfaclFile $TARGET_FILE
+
+echo "setfacl -x $USER:$USER2 $TARGET_FILE"
+setfacl -x $USER:$USER2 $TARGET_FILE
+
+listFile $TARGET_FILE
+getfaclFile $TARGET_FILE
+
+rm $TARGET_FILE
+sudo userdel -r 2>/dev/null $USER1
+sudo userdel -r 2>/dev/null $USER2
